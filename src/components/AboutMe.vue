1fe8fa4bfe2ed5958dfe8af751a1f50985df38d1
ryeonkyeongHong <rkhong@blackholic.com>
2022-05-12 13:34:34 +0900

    Feat: AboutMe 링크 추가

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -25,6 +25,10 @@
                   <i class="fas fa-piggy-bank"></i>
                   현재 VUE 기반의 모바일 쇼핑몰 신규 제작 작업을 하고 있습니다.
                 </p>   
+                <div class="btn_wrap">
+                  <a href="https://jungle-skateboard-a25.notion.site/75e61e8ffc3d4b42bb2b21a29190d3d8" target="_blank" class="btn_txt type4"><span>경력기술서 보기 ></span></a>
+                  <a href="https://velog.io/@rkhong" target="_blank" class="btn_txt type4"><span>블로그 보기 ></span></a>
+                </div>
               </div>              
             </div>
             
@@ -71,7 +75,7 @@
         name: 'AboutMe',
         data: function () {
           return {
-            typoTxt: '안녕하세요.  홍려경입니다.',
+            typoTxt: '안녕하세요.  홍려경입니다 :)',
             index: 0,
           }
         },
@@ -101,7 +105,7 @@
 
 <style lang="scss" scoped>  
   .sec_inner {text-align: center;}
-  .profile_wrap {display: inline-flex; align-items: center; justify-content: center;}
+  .profile_wrap {margin-top: 50px; display: inline-flex; align-items: center; justify-content: center;}
   .profile_img {width: 25%;}
   .tit {text-align: center;}
   .typo_txt {display: inline; border-right: 0.2em solid #222; animation: cursor 0.3s ease infinite;}
@@ -121,17 +125,38 @@
   .info_wrap dt:not(:first-child) {margin-top: 8px;}
   .info_wrap dd {color: #333; font-size:14px;}
   .info_wrap dd em {font-weight: bold; font-size: 16px;}
+  .btn_wrap {margin-top: 20px; display: flex; align-items: flex-end; justify-content: center; flex-direction: column;}
+  .btn_txt {
+	display:inline-block;position:relative;margin-right:5px;text-align:center;vertical-align:middle;font-size:13px;line-height:18px;letter-spacing:0.5px; color: #a1a1a1; font-weight: bold;
+	-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;
+	-webkit-transition-duration:.4s;transition-duration:.4s;
+}
+.btn_txt:after {background:#a1a1a1;}
+.btn_txt + .btn_txt {margin-top: 5px;}
    @media (max-width: 640px) {
     .info_wrap  {margin-top: 10%;}
     .info_wrap .info_items {padding: 7% 2%;}
     .info_items:not(:first-child) {margin-top: 5%;}
     .profile_img {display: none;}
-    .profile_txt {margin-top: 100px;}
+    .profile_txt {margin-top: 50px;}
     .profile_wrap > p {width: 100%;}
+    .btn_wrap {align-items: flex-start;}
   }
   @media all and (min-width: 1280px) {
   .profile_txt p {font-size: 16px;}
 	.info_wrap {flex-wrap: nowrap;}
 	.info_wrap .info_items {margin: 0; flex-basis: 49%;}
+  .btn_txt {font-size:18px;line-height:24px;}
+	.btn_txt:hover {color:#222;}
+	.btn_txt:after {
+		content:'';display:block;position:absolute;top:100%;left:0;right:0;height:1px;background:#000;
+		-webkit-transform:scaleX(0);transform:scaleX(0);
+		-webkit-transition:-webkit-transform .4s cubic-bezier(.7,0,.3,1);
+		transition:-webkit-transform .4s cubic-bezier(.7,0,.3,1);
+		transition:transform .4s cubic-bezier(.7,0,.3,1);
+		-webkit-transform-origin:right center;transform-origin:right center;
+	}
+	.btn_txt:hover:after {background:#222;}
+  .btn_txt.type4:hover:after {-webkit-transform:scaleX(1);transform:scaleX(1);-webkit-transform-origin:left center;transform-origin:left center;}
 }
 </style>
